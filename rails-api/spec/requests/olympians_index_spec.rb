a3dd7384124fac4b3641ccff10b7eb6b1ce288b2
fentontaylor <fentontaylor@gmail.com>
2020-01-28 15:56:01 -0700

    Add test for GET api/v1/olympians

--- rails-api/spec/requests/olympians_index_spec.rb
+++ rails-api/spec/requests/olympians_index_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 describe 'GET api/v1/olympians' do
   it 'returns an index of all olympians' do
-    o1 = create(:olympian)
+    oly1, oly2 = create(:olympian), create(:olympian)
 
+    get '/api/v1/olympians'
+
+    expect(response.status).to eq(200)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data).to eq([oly1, oly2])
   end
 end
