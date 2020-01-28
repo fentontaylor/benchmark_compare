0ce88e1b83367e1899e856ea6fb9bd389fa3d850
fentontaylor <fentontaylor@gmail.com>
2020-01-28 15:53:29 -0700

    Configure factory bot with faker

--- rails-api/spec/factories/olympian.rb
+++ rails-api/spec/factories/olympian.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
+  gender = ['M', 'F'].sample
+  first_name = gender == 'M' ? Faker::Name.male_first_name : Faker::Name.female_first_name
   factory :olympian do
-    name { "Bob" }
-    last_name { ('A'..'Z').to_a.sample + '.' }
+    name { first_name + ' ' + Faker::Name.last_name }
+    sex { gender }
+    age { (16..35).to_a.sample }
+    height { (150..200).to_a.sample }
+    weight { (40..115).to_a.sample }
   end
 end
--- rails-api/spec/requests/olympians_index_spec.rb
+++ rails-api/spec/requests/olympians_index_spec.rb
@@ -1,1 +1,8 @@
 require 'rails_helper'
+
+describe 'GET api/v1/olympians' do
+  it 'returns an index of all olympians' do
+    o1 = create(:olympian)
+
+  end
+end
