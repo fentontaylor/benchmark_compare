26eeef1147e35f42802b3771c30d6dfcf414e16c
fentontaylor <fentontaylor@gmail.com>
2020-01-28 16:01:28 -0700

    Add route and controller action for api/v1/olympians#index

---
+++ rails-api/app/controllers/api/v1/olympians_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::OlympiansController < ApplicationController
+  def index
+    
+  end
+end
--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: 'hello#index'
+  namespace :api do
+    namespace :v1 do
+      resources :olympians, only: [:index]
+    end
+  end
 end
