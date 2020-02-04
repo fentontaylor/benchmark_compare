1faa9b60e165c5a0f02aa63cdb7872a768a69ac9
Tylor Schafer <tylorschafer@gmail.com>
2020-02-03 20:31:08 -0700

    Add olympians url connection

--- django/api/urls.py
+++ django/api/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hello.urls'))
+    path('', include('hello.urls')),
+    path('api/v1/', include('olympians.urls'))
 ]
