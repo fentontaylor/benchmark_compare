34c1120b82be1c8731143280692f2df779cde5ce
Tylor Schafer <tylorschafer@gmail.com>
2020-02-04 13:13:59 -0700

    Refactor Repository name

--- spring/src/main/java/com/example/spring/repository/OlympianRepository.java
+++ spring/src/main/java/com/example/spring/repository/OlympianRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuestionRepository extends JpaRepository<Olympian, Long> {
+public interface OlympianRepository extends JpaRepository<Olympian, Long> {
 }
