b951a8923aedaecb75d72290ffe694e1dfcb107f
evettetelyas <etelyas@gmail.com>
2020-02-04 10:10:02 -0700

    updade env dev -> prod

--- phoenix/config/prod.secret.exs
+++ phoenix/config/prod.secret.exs
@@ -4,24 +4,20 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
-
 config :phoenix_benchmark, PhoenixBenchmark.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+  username: "postgres",
+  password: "postgres",
+  database: "benchmark_compare_dev",
+  hostname: "localhost",
+  show_sensitive_data_on_connection_error: true,
+	pool_size: 10
+	
+secret_key_base = "foo"
+  # System.get_env("SECRET_KEY_BASE") ||
+  #   raise """
+  #   environment variable SECRET_KEY_BASE is missing.
+  #   You can generate one by calling: mix phx.gen.secret
+  #   """
 
 config :phoenix_benchmark, PhoenixBenchmarkWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
