ce13c2f14e5eef94f1e04c2e3bb3aa7115477f4f
evettetelyas <etelyas@gmail.com>
2020-02-04 10:24:50 -0700

    remove changeset

--- phoenix/lib/phoenix_benchmark/olympians/olympian.ex
+++ phoenix/lib/phoenix_benchmark/olympians/olympian.ex
@@ -1,6 +1,5 @@
 defmodule PhoenixBenchmark.Olympians.Olympian do
 	use Ecto.Schema
-	import Ecto.Changeset
 	
 	schema "olympians" do
 		field :name, :string
@@ -9,9 +8,5 @@ defmodule PhoenixBenchmark.Olympians.Olympian do
 		field :height, :integer
 		field :weight, :integer
 	end
-  
-	def changeset(struct, params \\ %{}) do
-	  struct
-	  |> cast(params, [:name, :sex, :age, :height, :weight])
-	end
+
 end
