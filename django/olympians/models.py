bbdcdb9a3c9e81529bdf6469f420374ed4046b0c
Tylor Schafer <tylorschafer@gmail.com>
2020-02-03 20:03:16 -0700

    Add Olympian Model

--- django/olympians/models.py
+++ django/olympians/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Olympian(models.Model):
+    name = models.CharField(max_length = 250)
+    sex = models.CharField(max_length = 3)
+    age: models.IntegerField()
+    height: models.IntegerField()
+    weight: models.IntegerField()
+
+    def __str__(self):
+        return self.name
