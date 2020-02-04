54bbb4b04cbc398d25214a1267cf058412fe2117
Tylor Schafer <tylorschafer@gmail.com>
2020-02-03 20:32:27 -0700

    Fix integerfield for Olympians model and specify table name meta

--- django/olympians/models.py
+++ django/olympians/models.py
@@ -3,9 +3,12 @@ from django.db import models
 class Olympian(models.Model):
     name = models.CharField(max_length = 250)
     sex = models.CharField(max_length = 3)
-    age: models.IntegerField()
-    height: models.IntegerField()
-    weight: models.IntegerField()
+    age = models.IntegerField(blank=True, null=True)
+    height = models.IntegerField(blank=True, null=True)
+    weight = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        db_table = 'olympians'
