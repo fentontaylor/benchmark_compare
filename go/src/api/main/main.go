fa53782655c6ee48f17937f5c6eaeaefe0952203
Tylor Schafer <tylorschafer@gmail.com>
2020-02-07 15:44:00 -0700

    Add empty olympiansHandler and route

--- go/src/api/main/main.go
+++ go/src/api/main/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+  "log"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/api/v1/olympians", olympiansHandler)
+  log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func olympiansHandler(w http.ResponseWriter, r *http.Request) {
+  //
+}
