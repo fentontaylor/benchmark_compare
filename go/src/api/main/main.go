7557187801b98af2f0e9613f9a88134cdc01926a
Tylor Schafer <tylorschafer@gmail.com>
2020-02-07 15:57:56 -0700

    Fix dependecies bug

--- go/src/api/main/main.go
+++ go/src/api/main/main.go
@@ -5,7 +5,7 @@ import (
   "fmt"
   "log"
   "net/http"
-  "ox"
+  "os"
 
   _ "github.com/lib/pq"
 )
@@ -46,4 +46,34 @@ func initDb() {
         panic(err)
     }
     fmt.Println("Successfully connected!")
+}
+
+func dbConfig() map[string]string {
+    conf := make(map[string]string)
+    host, ok := os.LookupEnv(dbhost)
+    if !ok {
+        panic("DBHOST environment variable required but not set")
+    }
+    port, ok := os.LookupEnv(dbport)
+    if !ok {
+        panic("DBPORT environment variable required but not set")
+    }
+    user, ok := os.LookupEnv(dbuser)
+    if !ok {
+        panic("DBUSER environment variable required but not set")
+    }
+    password, ok := os.LookupEnv(dbpass)
+    if !ok {
+        panic("DBPASS environment variable required but not set")
+    }
+    name, ok := os.LookupEnv(dbname)
+    if !ok {
+        panic("DBNAME environment variable required but not set")
+    }
+    conf[dbhost] = host
+    conf[dbport] = port
+    conf[dbuser] = user
+    conf[dbpass] = password
+    conf[dbname] = name
+    return conf
 }
