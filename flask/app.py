f53aacf50611df6791f4fc0b511c550abf2a913c
fentontaylor <fentontaylor@gmail.com>
2020-02-07 15:02:14 -0700

    Update with pycodestyle changes and import changes

--- flask/app.py
+++ flask/app.py
@@ -2,25 +2,24 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_marshmallow import Marshmallow
+import models
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-from models import Olympian, OlympianSchema
-
 
 @app.route('/')
 def hello():
-    return jsonify({ 'message': 'Hello, World!' })
+    return jsonify({'message': 'Hello, World!'})
 
 
 @app.route('/api/v1/olympians')
 def index():
-    olympians = Olympian.query.all()
-    olympian_schema = OlympianSchema(many=True)
-    return { 'olympians': olympian_schema.dump(olympians) }
+    olympians = models.Olympian.query.all()
+    olympian_schema = models.OlympianSchema(many=True)
+    return {'olympians': olympian_schema.dump(olympians)}
 
 
 if __name__ == '__main__':
