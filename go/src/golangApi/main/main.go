357909b556fc5579133139eae5977d8b3890d754
Tylor Schafer <tylorschafer@gmail.com>
2020-02-08 11:55:22 -0700

    Add Hello World endpoint

--- go/src/golangApi/main/main.go
+++ go/src/golangApi/main/main.go
@@ -18,10 +18,22 @@ func main() {
   d.InitDb()
   fmt.Println("Database connection established")
   defer db.Close()
+  http.HandleFunc("/", helloWorldHandler)
   http.HandleFunc("/api/v1/olympians/", olympiansHandler)
   if err := http.ListenAndServe("localhost:8080", nil); err != nil {
     log.Fatal("ListenAndServe: ", err)
 }
+}
+
+func helloWorldHandler(w http.ResponseWriter, req *http.Request) {
+  output := make(map[string]string)
+  output["message"] = "Hello World!"
+  out, err := json.Marshal(output)
+  if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+  fmt.Fprintf(w, string(out))
 }
 
 func olympiansHandler(w http.ResponseWriter, req *http.Request) {
