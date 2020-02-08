cae257c65e169cd9f8dbd36a44262eb1870d7be4
fentontaylor <fentontaylor@gmail.com>
2020-02-08 14:48:57 -0700

    Add api/v1/olympians endpoint

--- sinatra/server.rb
+++ sinatra/server.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
 require 'json'
+require 'sequel'
+DB = Sequel.connect('postgres://localhost/benchmark_compare_dev')
 
 get '/' do 
   content_type :json
-  { message: 'Hello, World!' }.to_json
+  {message: 'Hello, World!'}.to_json
+end
+
+get '/api/v1/olympians' do
+  content_type :json
+  result = DB.from(:olympians).all
+  {olympians: result}.to_json
 end
